ce559c8212f8adbe309f4e565ed7274d7343fa9f
albertogeek7 <albertomedeiros777@gmail.com>
2022-07-02 17:58:40 -0300

    acho q tá pronto
    projeto praticamente finalizada

--- script.js
+++
@@ -1,7 +0,0 @@
-function instagran(img){ 
-    
-    var tela = document.getElementById('telaiphone')
-
-    tela.style.background = "url(rede/pacote-d013/pacote-d013/imagens/frame-instagran.jpg) no-repeat;"
-}
-
